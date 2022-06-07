2ebe3da55de67690e44dfd794f653ead743796a6
AgentIkko <97154134+AgentIkko@users.noreply.github.com>
2022-06-07 11:11:45 +0900

    Update genikko.py

--- genikko.py
+++ genikko.py
@@ -360,8 +360,9 @@ def semRelExpanderContent(kw,dv,sentlist):
         with col2:
             st.markdown(f"""
                 <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color:#e0f0d8;"> {sortedSimScores[0][0]}</div>
+                <p style = "margin-bottom: 4px"><p>
                 <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #e0f0d8"> {sortedSimScores[1][0]}</div>
-                <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #e0f0d8"> {sortedSimScores[2][0]}</div><br>
+                <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #e0f0d8"> {sortedSimScores[2][0]}</div>
                 <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #faeaea"> {sortedSimScores[-3][0]}</div>
                 <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #faeaea"> {sortedSimScores[-2][0]}</div>
                 <div style = "border-radius:10px 10px 10px 10px;text-align:center;background-color: #faeaea"> {sortedSimScores[-1][0]}</div>
