a08af4c4a68e03595cb6f816fbf01537e3cb4a91
AgentIkko <97154134+AgentIkko@users.noreply.github.com>
2022-06-07 12:45:53 +0900

    Update genikko.py

--- genikko.py
+++ genikko.py
@@ -604,7 +604,7 @@ if optionPhase == "原稿推薦表現":# and task_submitted:
                 with st.spinner("Processing..."):
                     listPresent = sorted(listKaigoStaff,key=lambda x:x[1],reverse=True)
                     listPresent = [e[0].strip() for e in listPresent]
-                    listPresent = sorted(set(listPrensent),key=listPresent.index)[:21]
+                    listPresent = sorted(set(listPresent),key=listPresent.index)[:21]
                     rawhtml = '</div><div style = "border-radius:4px 4px 4px 4px;text-align:start;background-color:#e0f0d8;"><p style = "margin-bottom: 0.5px"></p>'.join(listPresent)
                     st.markdown(f"""
                         <div style = "border-radius:4px 4px 4px 4px;text-align:start;background-color:#e0f0d8;">{rawhtml}</div>
@@ -613,7 +613,7 @@ if optionPhase == "原稿推薦表現":# and task_submitted:
                 with st.spinner("Processing..."):
                     listPresent = sorted(listSyufu,key=lambda x:x[1],reverse=True)
                     listPresent = [e[0].strip() for e in listPresent]
-                    listPresent = sorted(set(listPrensent),key=listPresent.index)[:21]
+                    listPresent = sorted(set(listPresent),key=listPresent.index)[:21]
                     rawhtml = '</div><div style = "border-radius:4px 4px 4px 4px;text-align:start;background-color:#e0f0d8;"><p style = "margin-bottom: 0.5px"></p>'.join(listPresent)
                     st.markdown(f"""
                         <div style = "border-radius:4px 4px 4px 4px;text-align:start;background-color:#e0f0d8;">{rawhtml}</div>
